99a11a51ed5a72c80e57ee94a116cf304959e804
Robin Stahl <stahl.ro@icloud.com>
2024-01-21 09:23:19 +0100

    add JavaFX

---
+++ src/main/java/module-info.java
@@ -0,0 +1,12 @@
+module com.example.swep2 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens com.example.swep2 to javafx.fxml;
+    exports com.example.swep2;
+}
